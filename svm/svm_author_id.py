62c441d6e1b3aec2377fdfd2a5841f52817629aa
Grigoriy Rozhkov <grigoriyroghkov@gmail.com>
2016-06-30 22:59:36 +0300

    SVM

--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -25,6 +25,41 @@ features_train, features_test, labels_train, labels_test = preprocess()
 #########################################################
 ### your code goes here ###
 
+from sklearn import svm
+from sklearn.metrics import accuracy_score
+
+# features_train = features_train[:len(features_train)/100]
+# labels_train = labels_train[:len(labels_train)/100]
+
+cls = svm.SVC(kernel="rbf", C=10000)
+
+t0 = time()
+cls.fit(features_train, labels_train)
+print "training time:", round(time()-t0, 3), "s"
+
+
+t0 = time()
+pred = cls.predict(features_test)
+print "predicting time:", round(time()-t0, 3), "s"
+
+# print "prediction for 10th: ", "Sara" if pred[10] == 0 else "Chris"
+# print "prediction for 26th: ", "Sara" if pred[26] == 0 else "Chris"
+# print "prediction for 50th: ", "Sara" if pred[50] == 0 else "Chris"
+
+match1 = 0
+match0 = 0
+for e in pred:
+    if e == 1:
+        match1 += 1
+    else:
+        match0 += 1
+
+print "Sara mails: ", str(match0)
+print "Chris mails: ", str(match1)
+
+accuracy = accuracy_score(labels_test, pred)
+print "accuracy: ", accuracy
+
 #########################################################
 
 
